3ff8bd929cd03d6d6e7db3b4e63f145cbfa5efb1
Shailee <shailee.madhani@gmail.com>
2021-06-24 20:38:55 -0700

    creating verson 1 app.py

--- app.py
+++ app.py
@@ -1,8 +1,40 @@
-from flask import Flask
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+#setup database engine for the flask application 
+engine = create_engine("sqlite:///hawaii.sqlite")
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+#get both the tables in the following variables
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#create a session link from python to database
+session = Session(engine)
+
+#following will create flask application called "app"
 app = Flask(__name__)
 
 #define the starting point, also known as the root
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route("/")
 
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
